0c17a5c7f3d89f32fe473e80fcf96567c9abf259
Liideli <roope.kasperi.laine@gmail.com>
2024-03-28 09:20:28 +0200

    user

---
+++ src/api/models/userModel.ts
@@ -0,0 +1,26 @@
+import mongoose from "mongoose";
+import {User} from '../../types/DBTypes';
+
+const userSchema = new mongoose.Schema<User>({
+  user_name: {
+    type: String,
+    minlength: [3, 'Username is required.'],
+    unique: true,
+  },
+  email: {
+    type: String,
+    required: [true, 'Email is required.'],
+    unique: true,
+  },
+  password: {
+    type: String,
+    minlength: [4, 'Password is required.'],
+  },
+  role: {
+    type: String,
+    enum: ['user', 'admin'],
+    default: 'user',
+  },
+});
+
+export default mongoose.model<User>('User', userSchema);
